c153f3497ebd5c68e88c0b93510a11cb996b812b
vaha77 <abdulvohid996@gmail.com>
2021-12-27 08:03:42 +0900

    vaxa

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,16 +1,72 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
-export const DiaryItem = ({ author, content, creadet_date, emotion, id }) => {
+export const DiaryItem = ({
+  onEdit,
+  onRemove,
+  author,
+  content,
+  creadet_date,
+  emotion,
+  id,
+}) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+  const [localContent, setLocalContent] = useState(content);
+  const hendelRemove = () => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제 하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+  const localContentInput = useRef();
+  const handelQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+  const handelEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current.focus();
+      return;
+    }
+    if (window.confirm(`${id}번째 일기를 수정하시겠습니까?`)) {
+      onEdit(id, localContent);
+      toggleIsEdit();
+    }
+  };
+
   return (
     <div className="DiaryItem">
       <div className="info">
         <span>
-          작성자 : {author} | 감정점수 : {emotion}
+          작성자 : {author} | 감정 : {emotion}
         </span>
         <br />
         <span className="date">{new Date(creadet_date).toLocaleString()} </span>
       </div>
-      <div className="content">{content} </div>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+
+      {isEdit ? (
+        <>
+          <button onClick={handelQuitEdit}>수정 취소</button>
+          <button onClick={handelEdit}>수정 완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={hendelRemove}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
+      )}
     </div>
   );
 };
--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,15 +1,14 @@
 import React from "react";
 import DiaryItem from "./DiaryItem";
 
-export const DiaryList = ({ diaryList }) => {
-  console.log(diaryList);
+export const DiaryList = ({ onEdit, onRemove, diaryList }) => {
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
       <h4>{diaryList.length}개외 일기가 있급니다.</h4>
       <div>
         {diaryList.map((it) => (
-          <DiaryItem key={it.id} {...it} />
+          <DiaryItem key={it.id} {...it}onEdit={onEdit} onRemove={onRemove} />
         ))}
       </div>
     </div>
