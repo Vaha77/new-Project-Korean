4693443e1af66d7c4ddcc6216e79d55391197fc2
vaha77 <abdulvohid996@gmail.com>
2022-01-02 23:53:43 +0900

    vaxa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
@@ -75,10 +75,24 @@ const App = () => {
       )
     );
   };
+  const getDairyAnalysis = useMemo(() => {
+    console.log("일기 분석 시작");
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDairyAnalysis;
 
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length} </div>
+      <div> 기분 좋은 일기 개수 : {goodCount} </div>
+      <div> 기분 나쁜 일기 개수 : {badCount} </div>
+      <div> 기분 좋은 일기 비율 : {goodRatio} </div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
